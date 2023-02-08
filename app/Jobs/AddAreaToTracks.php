7e71bdebb3571458b6b20af63af3ef9dfe3d1370
marco <m.baroncini@cyberway.cloud>
2023-02-08 10:05:59 +0100

    Add AddAreaToTracks job

---
+++ app/Jobs/AddAreaToTracks.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Jobs;
+
+use Wm\WmPackage\Jobs\AbstractProcessorJob;
+
+class AddAreaToTracks extends AbstractProcessorJob
+{
+  public function process($input)
+  {
+    //TODO: implements
+    sleep(15);
+    return [1, 2];
+  }
+}
