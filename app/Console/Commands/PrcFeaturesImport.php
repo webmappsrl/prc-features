31dd387554fad39131c2b59dac22593b6a4e72a8
Gennaro Manzo <gennaromanzo1993@gmail.com>
2023-03-21 15:59:26 +0100

    Created empty command for importing areas from taxonomy wheres

---
+++ app/Console/Commands/PrcFeaturesImport.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PrcFeaturesImport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'prc-features:import';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import Areas from geohub1';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+    }
+}
